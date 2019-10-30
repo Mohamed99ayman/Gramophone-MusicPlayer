43e2b76dd69187d8fe1cdda53fa030f635951b24
mohamed99ayman <mohamed99ayman250@gmail.com>
2019-10-30 18:44:56 +0200

    second

--- app/src/main/java/com/example/mediaplayer/SongAdapter.java
+++ app/src/main/java/com/example/mediaplayer/SongAdapter.java
@@ -18,7 +18,6 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.RequestOptions;
-
 import java.io.File;
 import java.util.ArrayList;
 
